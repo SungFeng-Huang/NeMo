- update flow for causal mask training - allow segmented agent data - fix metrics for eval w/ audio_len - fix chuncked flow_inference

cosyvoice/flow/flow.py
@@ -80,6 +80,9 @@ class MaskedDiffWithXvec(torch.nn.Module):
         h = self.encoder_proj(h)
         h, h_lengths = self.length_regulator(h, feat_len)
 
+        mask = (~make_pad_mask(feat_len)).to(h)
+        feat = F.interpolate(feat.unsqueeze(dim=1), size=h.shape[1:], mode="nearest").squeeze(dim=1)
+
         # get conditions
         conds = torch.zeros(feat.shape, device=token.device)
         for i, j in enumerate(feat_len):
@@ -87,10 +90,9 @@ class MaskedDiffWithXvec(torch.nn.Module):
                 continue
             index = random.randint(0, int(0.8 * j))
             conds[i, :index] = feat[i, :index]
+            if getattr(self, 'causal_mask', False):
+                mask[i, index:] = 0
         conds = conds.transpose(1, 2)
-
-        mask = (~make_pad_mask(feat_len)).to(h)
-        feat = F.interpolate(feat.unsqueeze(dim=1), size=h.shape[1:], mode="nearest").squeeze(dim=1)
 
         loss, _ = self.decoder.compute_loss(
             feat.transpose(1, 2).contiguous(),

nemo/collections/speechlm2/data/s2s_dataset.py
@@ -123,8 +123,17 @@ class DuplexS2SDataset(torch.utils.data.Dataset):
             target_texts = [
                 " ".join(s.text for s in cut.supervisions if s.speaker in self.output_roles) for cut in cuts
             ]
+            agent_seg = [
+                [
+                    (int(s.start * self.target_sample_rate) , int((s.start + s.duration) * self.target_sample_rate) )
+                    for s in cut.supervisions if s.speaker in self.output_roles
+                ] for cut in cuts
+            ]
+            agent_seg_texts = [
+                [s.text for s in cut.supervisions if s.speaker in self.output_roles] for cut in cuts
+            ]
 
-        else:
+        else: # blackwell
 
             sample_id = [cut.id for cut in cuts]
             (
@@ -140,7 +149,32 @@ class DuplexS2SDataset(torch.utils.data.Dataset):
                 cuts, self.tokenizer, self.frame_length, roles=self.input_roles, segment_type="user_segments"
             )
             target_texts = [" ".join(segment["text"] for segment in cut.custom["agent_segments"]) for cut in cuts]
+            agent_seg = [[(int(segment["start"] * self.target_sample_rate) , int((segment["end"]) * self.target_sample_rate) )
+                          for segment in cut.custom["agent_segments"]] for cut in cuts]
+            # Filter out agent segments that are longer than 10 seconds
+            filtered_agent_seg, filtered_agent_seg_texts = [], []
+            for cut in cuts:
+                filtered_segments = []
+                max_seg_dur = 0
+                for segment in cut.custom["agent_segments"]:
+                    duration_seconds = (segment["end"] - segment["start"])
+                    if duration_seconds < 10.0:
+                        filtered_segments.append(segment)
+                        max_seg_dur = max(max_seg_dur, duration_seconds)
+                if filtered_segments:
+                    # Sample segments if total duration is too large
+                    total_duration = len(filtered_segments) * max_seg_dur
+                    if total_duration > 50.0:  # If total duration exceeds 30 seconds
+                        # Sample a subset of segments to keep total duration reasonable
+                        max_segments = max(1, int(50.0 / max_seg_dur))
+                        filtered_segments = filtered_segments[:max_segments]
+                    
+                    filtered_agent_seg.append([(int(segment["start"] * self.target_sample_rate) , int((segment["end"]) * self.target_sample_rate) ) for segment in filtered_segments])
+                    filtered_agent_seg_texts.append([segment["text"] for segment in filtered_segments])
+            agent_seg = filtered_agent_seg
+            agent_seg_texts = filtered_agent_seg_texts
 
+        # agent_seg = [[(int(s.start * self.target_sample_rate) , int((s.start + s.duration) * self.target_sample_rate) ) for s in cut.supervisions if s.speaker in self.output_roles] for cut in cuts]
 
         return {
             "sample_id": sample_id,
@@ -152,7 +186,9 @@ class DuplexS2SDataset(torch.utils.data.Dataset):
             "target_token_lens": target_token_lens,
             "source_tokens": source_tokens,
             "source_token_lens": source_token_lens,
-            "target_texts": target_texts
+            "target_texts": target_texts,
+            "agent_seg": agent_seg,
+            "agent_seg_texts": agent_seg_texts
         }
 
 

nemo/collections/speechlm2/models/duplex_s2s_speech_decoder_model.py
@@ -149,6 +149,7 @@ class DuplexS2SSpeechDecoderModel(LightningModule, HFHubMixin):
         self.audio_decoder = AudioDecoder(config_path=flow_config, flow_ckpt_path=flow_checkpoint,
                                      hift_ckpt_path=hift_checkpoint, block_size=self.cfg.block_size)
         self.stream_inference = self.cfg.stream_inference
+        setattr(self.audio_decoder.flow, 'causal_mask', getattr(self.cfg, 'causal_mask', False))
 
         self.pad_args = self.cfg.pad_args
 
@@ -260,7 +261,20 @@ class DuplexS2SSpeechDecoderModel(LightningModule, HFHubMixin):
     def prepare_inputs(self, batch: dict, train=False):
 
 
-        target_audio_list = [sample[sample != 0].unsqueeze(0) for sample in batch["target_audio"]]
+        # target_audio_list = [sample[sample != 0].unsqueeze(0) for sample in batch["target_audio"]]
+        target_audio_list = [
+            audio[start:end].unsqueeze(0) if (audio != 0).sum() > 0 else torch.zeros(1, batch["target_audio"].shape[1]).to(audio.device)
+            for audio, segs in zip(batch["target_audio"], batch["agent_seg"])
+            for start, end in segs
+        ]
+        target_texts = [
+            text for texts in batch["agent_seg_texts"] for text in texts
+        ]
+        sample_id = [
+            f"{batch['sample_id'][i]}_{start}_{end}"
+            for i, segs in enumerate(batch["agent_seg"])
+            for start, end in segs
+        ]
 
         # left silence padding when training
         if self.pad_args is not None:
@@ -269,6 +283,7 @@ class DuplexS2SSpeechDecoderModel(LightningModule, HFHubMixin):
             else:
                 target_audio_list = [torch.nn.functional.pad(sample, (8448, 0), **self.pad_args) for sample in target_audio_list]
         
+        speech_signal = target_audio_list
         audio_lens = torch.tensor([sample.shape[1] for sample in target_audio_list], device=self.device)
 
         target_mel_list = [mel_spectrogram(sample) for sample in target_audio_list]
@@ -299,11 +314,14 @@ class DuplexS2SSpeechDecoderModel(LightningModule, HFHubMixin):
         speech_tokens = padded_tensor
 
         return {
+            "sample_id": sample_id,
+            "speech_signal": speech_signal,
             "audio_lens": audio_lens,
             "speech_feat": speech_feat,
             "speech_feat_len": speech_feat_lens,
             "speech_token": speech_tokens,
             "speech_token_len": speech_token_len,
+            "target_texts": target_texts,
             "embedding": torch.zeros(speech_tokens.size(0), 192).to(self.device)
         }
 
@@ -415,7 +433,7 @@ class DuplexS2SSpeechDecoderModel(LightningModule, HFHubMixin):
         #         m.eval()
         inputs = self.prepare_inputs(batch, train=True)
 
-
+        self.print(batch_idx, inputs['speech_feat'].shape, inputs['speech_feat_len'].sum().item(), inputs['speech_feat'].shape[0]*inputs['speech_feat'].shape[-1])
 
         loss = self.audio_decoder.flow(inputs, self.device)
 
@@ -534,23 +552,25 @@ class DuplexS2SSpeechDecoderModel(LightningModule, HFHubMixin):
 
                 os.makedirs(self.cfg.get('audio_save_path'), exist_ok=True)
                 for i in range(batch):
-                    torchaudio.save(f"{self.cfg.audio_save_path}/{name}_{i}_{dataset_batch['sample_id'][i]}.wav",
-                                    response_speech[i, start_idx:].unsqueeze(0).cpu(), 22050)
-                    response_speech[i, audio_lens[i]:] = 0
+                    torchaudio.save(f"{self.cfg.audio_save_path}/{name}_{inputs['sample_id'][i]}.wav",
+                                    response_speech[i, start_idx:audio_lens[i]].unsqueeze(0).cpu(), 22050)
+                    torchaudio.save(f"{self.cfg.audio_save_path}/{name}_{inputs['sample_id'][i]}_ref.wav",
+                                    inputs['speech_signal'][i][0, start_idx:].unsqueeze(0).cpu(), 22050)
 
                 pred_audios = resample(response_speech[:, start_idx:], 22050, 16000)
 
                 self.asr_bleu.update(
                     name=name,
-                    refs=dataset_batch["target_texts"],
+                    refs=inputs["target_texts"],
                     pred_audio=pred_audios,
-                    pred_audio_lens=torch.tensor(pred_audios.shape[1] / 22050 * 16000).repeat(batch).to(torch.long),
+                    pred_audio_lens=torch.tensor((audio_lens - start_idx) / 22050 * 16000).repeat(batch).to(torch.long),
                 )
 
                 self.mos.update(
                     name=name,
                     pred_audios=pred_audios,
                     tmp_dir=os.path.join(self.cfg.get('audio_save_path'), "tmp"),
+                    pred_audio_lens=torch.tensor((audio_lens - start_idx) / 22050 * 16000).repeat(batch).to(torch.long),
                 )
 
 

nemo/collections/speechlm2/modules/flow_inference.py
@@ -108,11 +108,15 @@ class AudioDecoder(torch.nn.Module): # from token to wav
         prev_mel = None
 
         prev_idx = 0
-        start_idx = block_size if pad_args is None else self.flow.encoder.block_size
+        start_idx = 0 if pad_args is None else self.flow.encoder.block_size
 
         for idx in range(start_idx, token.size(1), block_size):
-            # if idx>block_size: break
-            tts_token = token[:, prev_idx:idx]
+            # current block: idx ~ idx + block_size
+            # if padding: prev_idx = enc_block_size, 
+            # first block: 0 ~ enc_block_size + block_size
+            #   --> next prev_idx = enc_block_size + block_size
+            # other blocks: prev_idx ~ idx + block_size
+            tts_token = token[:, prev_idx:idx + block_size]
 
             # print(tts_token.size())
 
@@ -133,7 +137,7 @@ class AudioDecoder(torch.nn.Module): # from token to wav
 
             prev_mel = tts_mel
             prev_speech = tts_speech
-            prev_idx = idx
+            prev_idx = idx + block_size
             # print(tts_mel.size())
 
             tts_speechs.append(tts_speech)

nemo/collections/speechlm2/parts/metrics/asr_bleu.py
@@ -16,6 +16,7 @@ from collections import defaultdict
 import sacrebleu
 import torch
 from whisper_normalizer.english import EnglishTextNormalizer
+from whisper_normalizer.basic import BasicTextNormalizer
 
 from nemo.collections.asr.models import ASRModel
 from nemo.collections.common.parts.optional_cuda_graphs import WithOptionalCudaGraphs
@@ -36,11 +37,11 @@ class ASRBLEU:
         self.verbose = verbose
         if normalize:
             if normalizer is None:
-                self.normalizer = EnglishTextNormalizer()
+                self.normalizer = lambda x: BasicTextNormalizer()(EnglishTextNormalizer()(x)) 
             else:
-                self.normalizer = normalizer
+                self.normalizer = lambda x: BasicTextNormalizer()(normalizer(x))
         else:
-            self.normalizer = _identity
+            self.normalizer = BasicTextNormalizer()
 
         self._refs = defaultdict(list)
         self._hyps = defaultdict(list)
@@ -73,8 +74,10 @@ class ASRBLEU:
 
         for ref, asr_hyp in zip(refs, asr_hyps):
             asr_hyp = asr_hyp.text
-            self._refs[name].append([self.normalizer(ref)])
-            self._hyps[name].append(self.normalizer(asr_hyp))
+            asr_hyp = self.normalizer(asr_hyp)
+            ref = self.normalizer(ref)
+            self._refs[name].append([ref])
+            self._hyps[name].append(asr_hyp)
             if self.verbose:
                 asrb = sacrebleu.sentence_bleu(asr_hyp, [ref]).score
                 logging.info(f"[REF]\t{ref}\n[ASR]\t{asr_hyp} [{asrb:.2f}]")
@@ -83,7 +86,8 @@ class ASRBLEU:
         """Computes the final score and deallocates ASR and partial results."""
         corpus_metric = {}
         for name in self._refs.keys():
-            metric = torch.tensor(sacrebleu.corpus_bleu(self._hyps[name], self._refs[name]).score)
+            # use_effective_order=True is important for the case where sentence length is too short, which is the case for the short audio in the test set
+            metric = torch.tensor(sacrebleu.corpus_bleu(self._hyps[name], self._refs[name], use_effective_order=True).score)
             corpus_metric[f"asr_bleu_{name}"] = metric
         corpus_metric["asr_bleu"] = torch.stack(list(corpus_metric.values())).mean()
         self._refs.clear()

nemo/collections/speechlm2/parts/metrics/mos.py
@@ -27,9 +27,8 @@ class MOS:
 
     def __init__(self, verbose: bool = True):
         self.verbose = verbose
-
-
         self._score = defaultdict(list)
+        self.model = None
 
     def reset(self):
         torch.cuda.memory.empty_cache()
@@ -37,23 +36,32 @@ class MOS:
             self.model = utmosv2.create_model(pretrained=True)
         return self
 
-    def update(self, name, pred_audios, tmp_dir) -> None:
+    def update(self, name, pred_audios, tmp_dir, pred_audio_lens: torch.Tensor = None) -> None:
+        # Skip MOS calculation if model failed to load
+        if self.model is None:
+            logging.warning("MOS model not available, skipping MOS calculation")
+            return
+            
         local_rank = int(os.environ.get("LOCAL_RANK", 0))
-
         rank_tmp_dir = os.path.join(tmp_dir, f"rank_{local_rank}")
         os.makedirs(rank_tmp_dir, exist_ok=True)
 
+        if pred_audio_lens is None:
+            pred_audio_lens = [pred_audios.shape[1]] * pred_audios.shape[0]
 
+        # Save audio files for MOS evaluation
         for i, waveform in enumerate(pred_audios):
-
             waveform = waveform.detach().cpu()
-            torchaudio.save(f"{rank_tmp_dir}/sample_{i}.wav", waveform.unsqueeze(0), 16000)
+            torchaudio.save(f"{rank_tmp_dir}/sample_{i}.wav", waveform[:pred_audio_lens[i]].unsqueeze(0), 16000)
             if i > 1:
                 break
 
-        torch.distributed.barrier()
+        # Wait for all ranks to finish saving files
+        if torch.distributed.is_initialized():
+            torch.distributed.barrier()
 
-        all_mos = self.model.predict(input_dir=rank_tmp_dir)
+        with fp32_precision():  # Some NeMo ASR models weren't trained with bfloat16.
+            all_mos = self.model.predict(input_dir=rank_tmp_dir)
 
         mos_score = sum([item['predicted_mos'] for item in all_mos]) / len(all_mos)
         self._score[name].append(mos_score)
@@ -62,10 +70,16 @@ class MOS:
         corpus_metric = {}
 
         for name in self._score.keys():
-            metric = torch.tensor(self._score[name])
-            corpus_metric[f"mos_{name}"] = metric.mean()
+            if len(self._score[name]) > 0:
+                metric = torch.tensor(self._score[name])
+                corpus_metric[f"mos_{name}"] = metric.mean()
 
-        corpus_metric['mos'] = torch.stack(list(corpus_metric.values())).mean()
+        if corpus_metric:
+            corpus_metric['mos'] = torch.stack(list(corpus_metric.values())).mean()
+        else:
+            # Return default MOS if no scores were computed
+            corpus_metric['mos'] = torch.tensor(0.0)
+            
         self._score.clear()
         return corpus_metric
 